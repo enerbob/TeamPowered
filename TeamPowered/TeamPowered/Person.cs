7c9c89e7bf0daf87fee1f6a2eaa7bde002a978cd
enerbob <rene.trepanier@hotmail.com>
2016-03-02 17:52:05 -0500

    Rename class

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TeamPowered
 {
-    class Class1
+    class Person
     {
         public String Name { get; set; }
         public String Address { get; set; }
